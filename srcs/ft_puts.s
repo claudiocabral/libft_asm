408099c86199ebacf7c12a961b28aeca81e8e9c2
Claudio Cabral <cabral1349@gmail.com>
2019-03-03 18:14:57 +0100

    add newline to puts

--- srcs/ft_puts.s
+++ srcs/ft_puts.s
@@ -2,6 +2,10 @@
 %define STDOUT 0x1
 %define WRITE 0x4
 
+section .data
+newline:
+    .string db 10
+
 section .text
 global ft_puts
 extern ft_strlen
@@ -11,6 +15,11 @@ ft_puts:
     mov rsi, rdi
     mov rdi, STDOUT
     mov rdx, rax
+    mov rax, MACH_SYSCALL(WRITE)
+    syscall
+    mov rsi, [rel newline.string]
+    mov rdi, STDOUT
+    mov rdx, 0x1
     mov rax, MACH_SYSCALL(WRITE)
     syscall
     leave
